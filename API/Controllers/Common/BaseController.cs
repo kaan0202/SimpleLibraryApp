54d29ea842ca56e17a1cd3995aaf782e720f8551
Kaan <kaganakkaya2002@gmail.com>
2024-04-10 17:12:49 +0300

    BaseController oluşturuldu

---
+++ API/Controllers/Common/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Results.Common;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Common
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> NoDataResponse(IRequest<BaseResponse> request)
+        {
+            BaseResponse response = await _mediator.Send(request);
+            if(!response.Success) 
+              return BadRequest(response);
+            
+            return Ok(response);
+        }
+
+        public async Task<IActionResult> DataResponse<T>(IRequest<BaseDataResponse<T>> request) where T : class,new()
+        {
+            BaseDataResponse<T> response = await _mediator.Send(request);
+            
+            if(!response.Success)
+                return BadRequest(response.Message);
+
+            return Ok(response);
+        } 
+    }
+}
