06555fe98536f87e2d482b1dcb17a669f851e35b
Kaan <kaganakkaya2002@gmail.com>
2024-04-17 17:19:10 +0300

    UseStaticFiles middleware'i eklendi

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
 using API.Extensions;
+using Application;
+using Infrastracture;
 using Infrastracture.Filter;
+using Infrastracture.Services.Storage.Local.ETicaretAPI.Infrasracture.Services.Storage.Local;
+using Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigureInfrastractureServices();
+builder.Services.ConfigurePersistanceServices();
+builder.Services.AddStorageService<LocalStorage>();
 // Add services to the container.
 
 builder.Services.AddControllers(options =>
@@ -10,6 +17,7 @@ builder.Services.AddControllers(options =>
     options.Filters.Add<ValidateFilter>();
 }).ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -24,7 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthorization();
 app.ConfigureExceptionHandler();
 
