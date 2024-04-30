26379ea446910797ac7dac52871fcaf4e5c96d57
Kaan <kaganakkaya2002@gmail.com>
2024-04-30 20:15:40 +0300

    UnitOfWork yazıldı

---
+++ Persistance/EntityFramework/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Application.UnitOfWork;
+using Persistance.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.EntityFramework.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        readonly LibraryDbContext _libraryDbContext;
+        public UnitOfWork( LibraryDbContext libraryDbContext)
+        {
+            _libraryDbContext = libraryDbContext;
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+           return await _libraryDbContext.SaveChangesAsync();
+        }
+    }
+}
